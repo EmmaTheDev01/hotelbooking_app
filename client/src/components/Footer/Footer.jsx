c001b27dbfb359e6f85e5c32803f22bd12ee66cc
EmmaTheDev01 <worldoffictionrw@gmail.com>
2023-07-11 11:58:27 +0200

    Refreshing local files

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,122 +1,22 @@
-// import { fontWeight } from "@material-ui/system";
-import React from "react";
-import FooterWhite from "./FooterWhite";
-
-const input1 = {
-  width: "25%",
-  marginRight: "5px",
-  height: "44px",
-  border: "1px solid grey",
-  borderRadius: "4px",
-  fontSize: "20px",
-  fontWeight: "100",
-  paddingLeft: "8px",
-};
-const label = {
-  color: "black",
-  fontSize: "14px",
-  padding: "0",
-  marginLeft: "10px",
-};
-const button = {
-  backgroundColor: "rgb(0,53,128)",
-  border: "none",
-  borderRadius: "4px",
-  height: "46px",
-  padding: "13px",
-  color: "white",
-  fontSize: "18px",
-  fontWeight: "100",
-};
-const p1 = {
-  color: "var(--primary-color)",
-  fontSize: "30px",
-  fontWeight: "600",
-  padding: "0",
-  margin: "0",
-};
-const p2 = {
-  color: "black",
-  fontSize: "16px",
-  padding: "0",
-  margin: "5px",
-  marginBottom: "14px",
-};
-
-const link = {
-  color: "black",
-  fontSize: "14px",
-  fontWeight: "500",
-  textDecoration: "none",
-};
-
+import React from 'react'
+import FooterWhite from './FooterWhite'
+import './footer.css'
 const Footer = () => {
   return (
-    <>
-      <div
-        style={{
-          marginTop: "50px",
-          backgroundColor: "white",
-          padding: "30px",
-          textAlign: "center",
-          width: "100vw",
-          marginLeft: "-8px ",
-          overflowY: "hidden",
-        }}
-      >
-        <p style={p1}>Save time, save money!</p>
-        <p style={p2}>Sign up and we'll send the best deals to you</p>
-        <div style={{ padding: "10px" }}>
-          <input style={input1} type="text" placeholder=" Your email" />
-          <button style={button}>Subscribe</button>
-        </div>
-        <input type="checkbox" id="c1" />
-        <label style={label} forhtml="c1">
-          Send me ads from 250Heights app!
-        </label>
+    <div>
+      <div className='subscribe-section'>
+          <div className='contents'>
+            <h2>Subscribe to get our best deals</h2>
+            <p>Get discounts, deals,updates, rates and tips right into your inbox </p>
+            <div className='subscribe-inputs'>
+            <input type='text' className='subscribe-field' placeholder='Sign up with your Email address' />
+            <button className='subscribe-btn' type='submit'>Subscribe</button>
+            </div>
+          </div>
       </div>
-      <div
-        style={{
-          backgroundColor: "white",
-          paddingTop: "20px",
-          paddingBottom: "15px",
-          border: "none",
-          width: "100%",
-          marginLeft: "-8px ",
-          overflowY: "hidden",
-        }}
-      >
-        <div
-          style={{
-            width: "70%",
-            margin: "auto",
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <a href="/" style={link}>
-            Mobile version
-          </a>
-          <a href="/" style={link}>
-            Your account
-          </a>
-          <a href="/" style={link}>
-            Make changes online to your booking
-          </a>
-          <a href="/" style={link}>
-            Contact Customer Service
-          </a>
-          <a href="/" style={link}>
-            Become an affiliate
-          </a>
-          <a href="/" style={link}>
-            Become an affiliate
-          </a>
-        </div>
-      </div>
-      <FooterWhite />
-    </>
-  );
-};
+      <FooterWhite/>
+    </div>
+  )
+}
 
-export default Footer;
+export default Footer
--- client/src/components/search/SIdeSearch.jsx
+++ client/src/components/search/SIdeSearch.jsx
@@ -3,7 +3,7 @@ import { Form, FormGroup, ListGroup, ListGroupItem, Button } from "reactstrap";
 import './side-search.css'
 const SIdeSearch = () => {
     return (
-        <div className='side-search'> 
+        <div className='side-search'>
             <div className='search-form-side'>
                 <Form className="search-info-form">
                     <FormGroup>
@@ -29,18 +29,19 @@ const SIdeSearch = () => {
 
             <div className='search-bottom'>
                 <ListGroup>
+                    <h5>Service</h5>
                     <ListGroupItem>
-                        <span><input type='checkbox' id='' /> City Center</span>
-                        <span><input type='checkbox' id='' /> 4+ Rating</span>
-                        <span><input type='checkbox' id='' /> City Center</span>
-                        <span><input type='checkbox' id='' /> City Center</span>
+
+                        <span><input type='checkbox' id='' /> Hotels</span>
+                        <span><input type='checkbox' id='' /> Tours</span>
+                        <span><input type='checkbox' id='' /> Cars</span>
                     </ListGroupItem>
                     <h5>Accomodation type</h5>
                     <ListGroupItem>
                         <span><input type='checkbox' id='' /> Appartment</span>
                         <span><input type='checkbox' id='' /> Hotel</span>
                         <span><input type='checkbox' id='' /> Villa</span>
-                        <span><input type='checkbox' id='' /> Other Accomodation</span>
+                        <span><input type='checkbox' id='' /> Other accomodation</span>
                     </ListGroupItem>
                     <h5>Price per night</h5>
                     <ListGroupItem>
@@ -53,11 +54,12 @@ const SIdeSearch = () => {
                     <ListGroup />
                     <h5>Popular Searches</h5>
                     <ListGroupItem>
+                        <span><input type='checkbox' id='' /> City center</span>
                         <span><input type='checkbox' id='' /> Parking</span>
                         <span><input type='checkbox' id='' /> Restaurant</span>
                         <span><input type='checkbox' id='' /> Breakfast</span>
-                        <span><input type='checkbox' id='' /> Free Cancelation</span>
-                        <span><input type='checkbox' id='' /> Fitness Center</span>
+                        <span><input type='checkbox' id='' /> Free cancelation</span>
+                        <span><input type='checkbox' id='' /> Fitness center</span>
                     </ListGroupItem>
                     <ListGroup />
                     <h5>Price per night</h5>
